3cb3e620842d88a349257b6047fa7ebb30e19244
Arturo Venegas <50221545+arturovenegas@users.noreply.github.com>
2021-06-29 18:54:27 -0500

    Examen de Trigarante vacante Desarrollador Web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout/layout.component';
+import { NavbarComponent } from './dashboard/navbar/navbar.component';
+import { FooterComponent } from './dashboard/footer/footer.component';
+import { RetiroComponent } from './dashboard/retiro/retiro.component';
+import { DisponibleComponent } from './dashboard/disponible/disponible.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    NavbarComponent,
+    FooterComponent,
+    RetiroComponent,
+    DisponibleComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
